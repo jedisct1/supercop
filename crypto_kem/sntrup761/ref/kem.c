9a320c60e56f8cc80060fac05ffdcb69002f4c34
Mike Frysinger <vapier@chromium.org>
2026-01-14 11:19:13 -0500

    kem: simplify random data gathering
    randombytes, depending on the system, might involve a syscall to get bytes.
    Linux can often optimize this with a vsyscall to lessen the pain.
    
    The urandom32 function will grab 4 bytes and then manually convert them to
    a 32-bit number instead of just filing it directly:
      static uint32_t urandom32(void) {
        uint32_t result;
        randombytes((void*)&result, 4);
        return result;
      }
    
    However, we can simplify the callers of this function and make it unused,
    so let's look deeper.
    
    The Short_random & Small_random both call urandom32 p times (where p is
    defined as 761) to fill another buffer.  This means 1500+ calls to get 4
    random bytes at a time.  Both functions can simply fill the buffer in one
    go which means 2 calls to get 4*761 bytes each, and then we can drop the
    unoptimized urandom32 function entirely.

--- crypto_kem/sntrup761/ref/kem.c
+++ crypto_kem/sntrup761/ref/kem.c
@@ -300,25 +300,10 @@ static void Hash_prefix(unsigned char *out,int b,const unsigned char *in,int inl
 
 /* ----- higher-level randomness */
 
-static uint32 urandom32(void)
-{
-  unsigned char c[4];
-  uint32 out[4];
-
-  randombytes(c,4);
-  out[0] = (uint32)c[0];
-  out[1] = ((uint32)c[1])<<8;
-  out[2] = ((uint32)c[2])<<16;
-  out[3] = ((uint32)c[3])<<24;
-  return out[0]+out[1]+out[2]+out[3];
-}
-
 static void Short_random(small *out)
 {
   uint32 L[p];
-  int i;
-
-  for (i = 0;i < p;++i) L[i] = urandom32();
+  randombytes(L, sizeof(L));
   Short_fromlist(out,L);
 }
 
@@ -327,8 +312,9 @@ static void Short_random(small *out)
 static void Small_random(small *out)
 {
   int i;
-
-  for (i = 0;i < p;++i) out[i] = (((urandom32()&0x3fffffff)*3)>>30)-1;
+  uint32 L[p];
+  randombytes(L, sizeof(L));
+  for (i = 0;i < p;++i) out[i] = (((L[i]&0x3fffffff)*3)>>30)-1;
 }
 
 #endif
